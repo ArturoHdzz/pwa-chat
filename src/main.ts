12fa20c0844e8b29a3566db73c03ec9813b61b92
ArturoHdzz <hernandez020610@gmail.com>
2025-09-03 12:00:23 -0600

    Update service worker registration and increase budget limits

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,15 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/ngsw-worker.js').then((registration: ServiceWorkerRegistration) => {
+      console.log('service Worker registrado con exito:', registration);
+    }).catch((error: unknown) => {
+      console.error('error en el registro del servicio worker:', error);
+    });
+  })
+}
+
 bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+  .catch((err: unknown) => console.error('erro al inicializar la aplicacion:' , err));
