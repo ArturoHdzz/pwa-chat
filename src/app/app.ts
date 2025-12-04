d3a0259cceb2d4a5872dadfe429dc26171a285bb
Jesus Arturo Hernandez Cristan <hernandez020610@gmail.com>
2025-12-03 22:17:09 -0600

    actulizacion

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,33 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { filter } from 'rxjs';
+
 @Component({
   selector: 'app-root',
   imports: [
     IonApp, RouterOutlet,
-
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('pwa');
+  private swUpdate = inject(SwUpdate);
+
+  ngOnInit() {
+    if (this.swUpdate.isEnabled) {
+      
+      this.swUpdate.versionUpdates
+        .pipe(
+          filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY')
+        )
+        .subscribe(() => {
+          if (confirm('¡Nueva actualización disponible! ¿Deseas recargar la aplicación para ver los cambios?')) {
+            window.location.reload();
+          }
+        });
+    }
+  }
 }
