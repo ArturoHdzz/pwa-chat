090f5e635b24423d571ade7774aed74829bef22d
VictoriaReyes <victoria.jaime@silverstone-tech.com>
2025-12-04 16:53:35 -0600

    server

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://127.0.0.1:8000/api',
+  apiUrl: 'https://pwa-back.zeabur.app/api',
   vapidPublicKey:'BOqrrj_KZNDkc-B6lOs7E0cXuDEGZ_QTjthoq9tMK3TcNRxTpRWuIq9Wi_UWIjJfD-Kkef1oWpdKezERjNI8Q2s'
 };
